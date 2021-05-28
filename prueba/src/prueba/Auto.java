400a67bf088bf33360e60d13a88038cebc9bb87f
Usuario <Usuario@DESKTOP-UH08QNP>
2021-05-28 20:11:29 -0300

    primer commit prueba

---
+++ prueba/src/prueba/Auto.java
@@ -0,0 +1,31 @@
+package prueba;
+
+public class Auto {
+
+	private Integer puerta;
+	private Double costo;
+	
+	public Auto(Integer puerta, Double costo) {
+		super();
+		this.puerta = puerta;
+		this.costo = costo;
+	}
+
+	public Integer getPuerta() {
+		return puerta;
+	}
+
+	public void setPuerta(Integer puerta) {
+		this.puerta = puerta;
+	}
+
+	public Double getCosto() {
+		return costo;
+	}
+
+	public void setCosto(Double costo) {
+		this.costo = costo;
+	}
+	
+	
+}
